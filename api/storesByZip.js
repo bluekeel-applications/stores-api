ad4cd52fa0dbdef8495c404c27c1ee20b9fac99a
Weston Gibler <weston@bluekeel.com>
2019-08-28 10:58:52 -0600

    updated the response object to be in the geoJson format needed for mapbox

--- api/storesByZip.js
+++ api/storesByZip.js
@@ -8,21 +8,51 @@ AWS.config.update({ region: 'us-east-1' });
 const util = require('./util.js');
 
 const urlBase = 'https://api.bestbuy.com/v1/stores(area(';
-const urlButt = ',50))?format=json&show=storeId,longName,name,phone,distance,address,address2,city,region,postalCode&pageSize=5&apiKey='
+const urlButt = ',100))?format=json&show=storeId,storeType,location,longName,name,phone,distance,address,address2,city,region,postalCode,country,lat,lng&pageSize=20&apiKey='
 const apiKey = process.env.BESTBUY_PRODUCTS_API_KEY;
+
+const formattedPhone = (rawPhone) => {
+    let parts = rawPhone.split('-');
+    return '(' + parts[0] + ') ' + parts[1] + '-' + parts[2];
+};
 
 exports.handler = async (event) => {
     try {
         let zipCode = event.pathParameters.zip;
         let url = urlBase + zipCode + urlButt + apiKey;
 
-        const response = await axios.get(url);
-        
+        let featureArray = [];
+        await axios.get(url).then(res => {
+            res.data.stores.forEach((store) => {
+                let storeGeoJson = {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [store.lat, store.lng]
+                    },
+                    properties: {
+                        phoneFormatted: formattedPhone(store.phone),
+                        phone: store.phone,
+                        address: store.address,
+                        address2: store.address2,
+                        city: store.city,
+                        country: store.country,
+                        crossStreet: store.location,
+                        postalCode: store.postalCode,
+                        state: store.region,
+                        storeId: store.storeId,
+                        distance: store.distance
+                    }
+                }
+                featureArray.push(storeGeoJson);
+            })
+        });
+
         return {
             statusCode: 200,
             headers: util.getResponseHeaders(),
             body: JSON.stringify({
-                stores: response.data.stores
+                stores: featureArray
             })
         };
     } catch (err) {
